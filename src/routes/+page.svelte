177ae8b9bb63e4c36973fc43788cd6727ef8913b
ribo916 <richard.boesch@gmail.com>
2024-10-11 14:18:05 -0700

    minor fixes to help ide stop complaining

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,28 +116,6 @@
 </script>
 
 <style>
-  /* Include the spinner CSS here if not already in your main stylesheet */
-  .spinner-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 100%;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5); /* Optional: Add a semi-transparent background */
-  }
-
-  .spinner {
-    border: 8px solid #f3f3f3; /* Light grey */
-    border-top: 8px solid #3498db; /* Blue */
-    border-radius: 50%;
-    width: 60px;
-    height: 60px;
-    animation: spin 2s linear infinite;
-  }
-
   @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
